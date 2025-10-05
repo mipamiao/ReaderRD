4141914e50d15d756ebb8c2ce420668586a5de42
mipa <3440171740@qq.com>
2025-10-05 19:31:05 +0800

    BookService的异常处理完成

--- common/src/main/java/com/mipa/common/Constant/ExMsg.java
+++ common/src/main/java/com/mipa/common/Constant/ExMsg.java
@@ -7,11 +7,36 @@ public class ExMsg {
     public final static String EXIST = "已经存在";
     public final static String NOT_EXIST = "不存在";
 
+    public final static String AND = "和";
+    public final static String OR = "或";
+
     public final static String USERNAME_EXIST = "用户名" + EXIST;
 
     public final static String USER_NOT_EXIST = "用户" + NOT_EXIST;
-    public  final static String BOOK_NOT_EXIST = "书籍不存在";
+    public final static String BOOK_NOT_EXIST = "书籍不存在";
     public final static String CHAPTER_NOT_EXIST = "章节不存在";
 
     public final static String USER_OR_PWD_WRONG = "用户名或密码错误";
+    public final static String NOT_AUTHOR = "不是本书作者";
+
+    public final static String EMPTY_FILE = "文件为空";
+    public final static String FILE_SAVE_ERROR = "文件存储出错";
+
+    public static String And(String ...strs){
+        var stringBuilder = new StringBuilder();
+        for(String str: strs){
+            if(!stringBuilder.isEmpty())stringBuilder.append(AND);
+            stringBuilder.append(str);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String Or(String ...strs){
+        var stringBuilder = new StringBuilder();
+        for(String str: strs){
+            if(!stringBuilder.isEmpty())stringBuilder.append(OR);
+            stringBuilder.append(str);
+        }
+        return stringBuilder.toString();
+    }
 }
